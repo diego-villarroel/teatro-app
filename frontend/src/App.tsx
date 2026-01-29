6e969056c792372c1bec1875f0fa8a84bc06be22
dvillarroel <diego.n.villarroel@gmail.com>
2026-01-28 21:55:50 -0300

    Dashboard para Gerente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,42 +7,6 @@ import { TeatroDash } from './components/TeatroDash/TeatroDash';
 import { AdminAudioElements } from './components/AdminAudioElements/AdminAudioElements';
 
 function App() {
-  const teatro = {
-    nombre:'San Martin', 
-    mics: [{
-      marca:'Shure',
-      modelo:'SM 58',
-      cantidad: 10,
-      estado: 4
-    },{
-      marca:'Shure',
-      modelo:'SM 57',
-      cantidad: 10,
-      estado: 4
-    }], 
-    consolas: [{
-      marca:'Yamaha',
-      modelo:'CL 5',
-      cantidad: 2,
-      estado: 3
-    },{
-      marca:'Yamaha',
-      modelo:'PM 3',
-      cantidad: 1,
-      estado: 4
-    }], 
-    cajas: [{
-      marca:'JBL',
-      modelo:'100',
-      cantidad: 4,
-      estado: 3
-    },{
-      marca:'JBL',
-      modelo:'200',
-      cantidad: 6,  
-      estado: 2
-    }], 
-  };
   return (
     <div className="container">
       <header>
@@ -54,7 +18,7 @@ function App() {
           <Routes>
             <Route path='/' element={<Dashboard></Dashboard>}>
             </Route>
-            <Route path='/admin-teatro' element={<TeatroDash teatroData={teatro} ></TeatroDash>}>
+            <Route path='/admin-teatro/teatro/:teatroId' element={<TeatroDash></TeatroDash>}>
             </Route>
             <Route path='/audio-elem' element={<AdminAudioElements></AdminAudioElements>}></Route>
           </Routes>
--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -1,18 +1,19 @@
+import { useEffect, useState } from "react";
 import { TeatroCard } from "../TeatrosCard/TeatroCard";
+import { getTeatros } from "../../services/teatros";
 import './dashboard.css'
 
 export const Dashboard = () => {
-  const teatros = [
-    {nombre:'San Martin', mics:20, consolas:5, cajas:10, otros:7, personal:17},
-    {nombre:'Alvear', mics:20, consolas:5, cajas:10, otros:7, personal:7},
-    {nombre:'Regio', mics:20, consolas:5, cajas:10, otros:7, personal:7},
-    {nombre:'La Rivera', mics:20, consolas:5, cajas:10, otros:7, personal:7},
-    {nombre:'Sarmiento', mics:20, consolas:5, cajas:10, otros:7, personal:7},
-  ]
+  const [teatros, setTeatros] = useState([]);
+  useEffect(() => {
+    getTeatros().then((teatros) => {
+      setTeatros(teatros);
+    });
+  }, []);
   return (
     <div className="dash">
-			{teatros.map((teatro, index) => (
-				<TeatroCard key={index} teatro={teatro}></TeatroCard>
+			{teatros.map((teatro: any) => (        
+				<TeatroCard key={teatro.id} teatro={teatro}></TeatroCard>
 			))}
     </div>
   );
